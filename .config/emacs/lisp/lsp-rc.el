63dc2a4ccc98a01934f0b87e1216d7857a4c2d74
beedware <beed.epistle@outlook.com>
2026-09-15 18:44:22 +0100

    fix: reduce eldoc buffer height

--- .config/emacs/lisp/lsp-rc.el
+++ .config/emacs/lisp/lsp-rc.el
@@ -8,6 +8,11 @@
                           (setq-local eldoc-display-functions
                                       '(eldoc-display-in-buffer)))))
 
+(add-to-list 'display-buffer-alist
+             '("\\*eldoc"
+               (display-buffer-reuse-window display-buffer-at-bottom)
+               (window-height . 0.2)))
+
 (defun beed/lsp-enable (hook server)
   "Enable Eglot on HOOK using SERVER."
   (add-hook hook #'eglot-ensure)
