8feaaaee45d6dd2f3d399b69a0865e4cf405d59a
beedware <beed.epistle@outlook.com>
2026-09-17 20:55:29 +0100

    feat: show trailing whitespace in Emacs

--- .config/emacs/lisp/ui-rc.el
+++ .config/emacs/lisp/ui-rc.el
@@ -15,12 +15,59 @@
   :ensure nil
   :defer nil
   :custom
-  (whitespace-style '(face trailing tabs tab-mark nbsp nbsp-mark))
-  (whitespace-display-mappings
-   '((tab-mark 9 [?> ?\s ?\s ?\s] [?> ?\s ?\s ?\s])
-     (nbsp-mark 160 [?+] [?+])))
+  (whitespace-style '(face tabs tab-mark nbsp nbsp-mark))
   :config
   (global-whitespace-mode 1))
+
+(defvar-local beed/trailing-space-overlays nil)
+
+(defface beed/trailing-space-marker
+  '((t :inherit shadow))
+  "Face used for trailing-space markers.")
+
+(defun beed/set-trailing-space-marker-face ()
+  (when (featurep 'modus-themes)
+    (set-face-attribute
+     'beed/trailing-space-marker nil
+     :foreground (modus-themes-generate-color-blend
+                  (modus-themes-get-color-value 'fg-dim)
+                  (modus-themes-get-color-value 'bg-main)
+                  0.55)
+     :background 'unspecified)))
+
+(defun beed/clear-trailing-space-overlays ()
+  (mapc #'delete-overlay beed/trailing-space-overlays)
+  (setq beed/trailing-space-overlays nil))
+
+(defun beed/show-trailing-spaces-as-dots (&rest _)
+  (beed/clear-trailing-space-overlays)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward " +$" nil t)
+      (let ((overlay (make-overlay (match-beginning 0) (match-end 0))))
+        (overlay-put overlay 'display
+                     (make-string (- (match-end 0) (match-beginning 0))
+                                  ?·))
+        (overlay-put overlay 'face 'beed/trailing-space-marker)
+        (push overlay beed/trailing-space-overlays)))))
+
+(define-minor-mode beed/trailing-space-mode
+  "Display trailing spaces as middle dots."
+  :lighter nil
+  (if beed/trailing-space-mode
+      (progn
+        (add-hook 'after-change-functions #'beed/show-trailing-spaces-as-dots nil t)
+        (beed/show-trailing-spaces-as-dots))
+    (remove-hook 'after-change-functions #'beed/show-trailing-spaces-as-dots t)
+    (beed/clear-trailing-space-overlays)))
+
+(define-globalized-minor-mode global-beed/trailing-space-mode
+  beed/trailing-space-mode
+  (lambda ()
+    (unless (minibufferp)
+      (beed/trailing-space-mode 1))))
+
+(global-beed/trailing-space-mode 1)
 
 (use-package modus-themes
   :demand t
@@ -29,7 +76,9 @@
          modus-themes-italic-constructs nil)
   (set-face-attribute 'line-number nil :slant 'normal)
   (set-face-attribute 'line-number-current-line nil :slant 'normal)
-  (load-theme 'modus-vivendi t))
+  (load-theme 'modus-vivendi t)
+  (beed/set-trailing-space-marker-face)
+  (add-hook 'enable-theme-functions #'beed/set-trailing-space-marker-face))
 
 (use-package doom-modeline
   :defer nil
