4c40b284ac4589833594af577911e5a19bf43629
beedware <beed.epistle@outlook.com>
2026-09-19 00:11:11 +0100

    feat: use project Python virtualenvs in Emacs

--- .config/emacs/lisp/python-rc.el
+++ .config/emacs/lisp/python-rc.el
@@ -1,5 +1,8 @@
 ;;; python-rc.el --- Python configuration -*- lexical-binding: t; -*-
 
+(require 'project)
+(require 'seq)
+
 (use-package python
   :ensure nil
   :mode ("\\.py\\'" . python-ts-mode)
@@ -7,6 +10,26 @@
 
 (beed/lsp-enable 'python-ts-mode-hook
                  '(python-ts-mode . ("ty" "server")))
+
+(defun beed/python-use-project-venv ()
+  "Use a project-local Python virtual environment when present."
+  (when-let* ((project (project-current))
+              (root (project-root project))
+              (venv (seq-find #'file-directory-p
+                              (mapcar (lambda (directory)
+                                        (expand-file-name directory root))
+                                      '(".venv" "venv")))))
+    (setq-local python-shell-virtualenv-root venv)
+    (setq-local exec-path (cons (expand-file-name "bin" venv) exec-path))
+    (setq-local process-environment
+                (cons (concat "VIRTUAL_ENV=" venv)
+                      (cons (concat "PATH="
+                                    (expand-file-name "bin" venv)
+                                    path-separator
+                                    (getenv "PATH"))
+                            process-environment)))))
+
+(add-hook 'python-ts-mode-hook #'beed/python-use-project-venv)
 
 (beed/formatter-add 'python-ts-mode '(ruff-isort ruff))
 
