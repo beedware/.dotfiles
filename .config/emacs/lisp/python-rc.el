c88d7601713fb42a9272171ebcd6ab3fe421799c
beedware <beed.epistle@outlook.com>
2026-09-19 00:22:44 +0100

    feat: add Python virtualenv LSP restart command

--- .config/emacs/lisp/python-rc.el
+++ .config/emacs/lisp/python-rc.el
@@ -2,6 +2,7 @@
 
 (require 'project)
 (require 'seq)
+(require 'subr-x)
 
 (use-package python
   :ensure nil
@@ -11,6 +12,49 @@
 (beed/lsp-enable 'python-ts-mode-hook
                  '(python-ts-mode . ("ty" "server")))
 
+(defun beed/python-set-venv (venv)
+  "Use VENV for Python tools in the current buffer."
+  (interactive "DVirtualenv: ")
+  (let* ((venv (file-name-as-directory (expand-file-name venv)))
+         (bin (expand-file-name "bin" venv))
+         (old-bin (when (bound-and-true-p python-shell-virtualenv-root)
+                    (expand-file-name "bin" python-shell-virtualenv-root))))
+    (unless (file-directory-p bin)
+      (user-error "%s does not look like a Python virtualenv" venv))
+    (setq-local python-shell-virtualenv-root venv)
+    (setq-local exec-path
+                (cons bin
+                      (seq-remove (lambda (path)
+                                    (member path (list bin old-bin)))
+                                  exec-path)))
+    (setq-local process-environment (copy-sequence process-environment))
+    (setenv "VIRTUAL_ENV" venv)
+    (setenv "PATH"
+            (string-join (cons bin
+                               (seq-remove (lambda (path)
+                                             (member path (list bin old-bin "")))
+                                           (split-string (or (getenv "PATH") "")
+                                                         path-separator)))
+                         path-separator))))
+
+(defun beed/python-set-venv-restart-lsp (venv)
+  "Use VENV for Python tools and restart Eglot in the current buffer."
+  (interactive
+   (list (read-directory-name
+          "Virtualenv: "
+          (or (when-let* ((project (project-current))
+                          (root (project-root project)))
+                (seq-find #'file-directory-p
+                          (mapcar (lambda (directory)
+                                    (expand-file-name directory root))
+                                  '(".venv" "venv"))))
+              default-directory))))
+  (beed/python-set-venv venv)
+  (require 'eglot)
+  (when (eglot-current-server)
+    (eglot-shutdown (eglot-current-server)))
+  (eglot-ensure))
+
 (defun beed/python-use-project-venv ()
   "Use a project-local Python virtual environment when present."
   (when-let* ((project (project-current))
@@ -19,15 +63,7 @@
                               (mapcar (lambda (directory)
                                         (expand-file-name directory root))
                                       '(".venv" "venv")))))
-    (setq-local python-shell-virtualenv-root venv)
-    (setq-local exec-path (cons (expand-file-name "bin" venv) exec-path))
-    (setq-local process-environment
-                (cons (concat "VIRTUAL_ENV=" venv)
-                      (cons (concat "PATH="
-                                    (expand-file-name "bin" venv)
-                                    path-separator
-                                    (getenv "PATH"))
-                            process-environment)))))
+    (beed/python-set-venv venv)))
 
 (add-hook 'python-ts-mode-hook #'beed/python-use-project-venv)
 
