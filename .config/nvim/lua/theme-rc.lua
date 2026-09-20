9624d6cde52f9a7997fa1b307cbbbf274f13d22b
beedware <beed.epistle@outlook.com>
2026-09-20 03:12:38 +0100

    feat: have the color of quicker be black

--- .config/nvim/lua/theme-rc.lua
+++ .config/nvim/lua/theme-rc.lua
@@ -10,6 +10,7 @@ require("modus-themes").setup {
   on_highlights = function(highlights, colors)
     highlights.NeogitActiveItem = { bg = colors.bg_dim, fg = colors.fg_main }
     highlights.TabLineSel = { bg = colors.bg_alt, fg = colors.fg_main }
+    highlights.QuickFixLineNr = { fg = colors.fg_main, bg = colors.none }
   end,
 }
 
