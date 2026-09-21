b3cd0a6d27486e85cdc09d47116072827d47e019
beedware <beed.epistle@outlook.com>
2026-09-21 14:50:36 +0100

    fix: resolve org capture journal files

--- .config/emacs/lisp/journal-rc.el
+++ .config/emacs/lisp/journal-rc.el
@@ -1,10 +1,29 @@
 ;;; journal-rc.el --- Journal configuration -*- lexical-binding: t; -*-
 
+(defconst beed/journal-directory
+  (file-truename (expand-file-name "~/Compendium/Journal/")))
+
+(defun beed/journal-file (file)
+  "Return FILE inside `beed/journal-directory'."
+  (expand-file-name file beed/journal-directory))
+
 (defconst beed/journal-capture-file
-  (expand-file-name "~/Compendium/Journal/repo.org"))
+  (beed/journal-file "repo.org"))
 
 (defconst beed/journal-inbox-file
-  (expand-file-name "~/Compendium/Journal/inbox.org"))
+  (beed/journal-file "inbox.org"))
+
+(defconst beed/journal-agenda-file-names
+  '("inbox.org"
+    "goals.org"
+    "marks.org"
+    "media.org"
+    "repo.org"
+    "kcl.org"
+    "hijri.org"
+    "planner.org"
+    "done.org"
+    "wishlist.org"))
 
 (defun beed/org-journal-capture-target ()
   "Capture under today's heading in `beed/journal-capture-file'."
@@ -25,42 +44,28 @@
   :ensure nil
   :after org
   :config
-  (setq org-directory
-        (file-truename
-         (expand-file-name "~/Compendium/Journal/")))
+  (setq org-directory beed/journal-directory)
 
   (setq org-agenda-files
-        (mapcar (lambda (file)
-                  (expand-file-name file org-directory))
-                '("inbox.org"
-                  "goals.org"
-                  "marks.org"
-                  "media.org"
-                  "repo.org"
-                  "kcl.org"
-                  "hijri.org"
-                  "planner.org"
-                  "done.org"
-                  "wishlist.org")))
+        (mapcar #'beed/journal-file beed/journal-agenda-file-names))
 
-  (setq org-default-notes-file
-        (expand-file-name "repo.org" org-directory))
+  (setq org-default-notes-file beed/journal-capture-file)
 
   (setq org-capture-templates
-         '(("n" "Note" entry
+        `(("n" "Note" entry
             (function beed/org-journal-capture-target)
             "** %<%H%M%S> - %?")
            ("i" "Inbox" entry
-            (file beed/journal-inbox-file)
+            (file ,beed/journal-inbox-file)
             "* %?")
            ("m" "Mark" entry
-            (file (expand-file-name "marks.org" org-directory))
+            (file ,(beed/journal-file "marks.org"))
             "* %^{Title} %^{Tags}\n:PROPERTIES:\n:URL: %^{URL}\n:END:\n\n%^{Description}%?")
            ("t" "Task" entry
-            (file (expand-file-name "planner.org" org-directory))
+            (file ,(beed/journal-file "planner.org"))
             "* TODO %^{Title} %^{Tags}\n%?")
            ("e" "Event" entry
-            (file (expand-file-name "planner.org" org-directory))
+            (file ,(beed/journal-file "planner.org"))
             "* %^{Title} %^{Tags}\n%?")))
 
   (setq org-refile-targets '((org-agenda-files :maxlevel . 2))
